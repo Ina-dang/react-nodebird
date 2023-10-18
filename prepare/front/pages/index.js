192e9fa340927b527722e2e009137d328d48090b
INA nh.lee <ina9377@gmail.com>
2023-10-18 16:18:41 +0900

    npm run dev로 next 첫실행

---
+++ prepare/front/pages/index.js
@@ -0,0 +1,8 @@
+/**
+ * 첫 랜더페이지 (구버전 Next는 Pages 안에 있는 컴포넌트들을 코드스플리팅된 각 페이지로 매핑한다)
+ * @returns React.Componenet
+ */
+const Home = () => {
+  return <div>Hello, Next!</div>;
+};
+export default Home;
