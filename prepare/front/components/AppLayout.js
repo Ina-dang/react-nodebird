3b94fc6c29e80270367598518ecefe72b0737073
INA nh.lee <ina9377@gmail.com>
2023-10-18 18:06:43 +0900

    grid 추가

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Input, Menu, Row, Col } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
@@ -17,10 +17,30 @@ const AppLayout = ({ children }) => {
           <Link href='/profile'>프로필</Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href='/signup'>회원가입</Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        {/*브레이크포인트: xs 모바일 <576,  sm 태블릿 >= 576, md 작은데스크탑 >=768 lg: 큰화면 >=992*/}
+        <Col xs={24} md={6}>
+          왼쪽메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href='https://h-owo-ld.tistory.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Made by Inadang
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
