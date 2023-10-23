1d9827439976948d127ac0107d72eee3049a829a
INA nh.lee <ina9377@gmail.com>
2023-10-23 21:03:20 +0900

    게시글 아이콘 추가 및 구현하기

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,7 +1,87 @@
-import React from 'react';
+import { Avatar, Button, Card, Popover } from 'antd';
+import React, { useCallback, useState } from 'react';
+import {
+  RetweetOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+  HeartTwoTone,
+} from '@ant-design/icons';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import PostImages from './PostImages';
 
-const PostCard = () => {
-  return <div>PostCard</div>;
+const PostCard = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+  const id = useSelector((state) => state.user.me && state.user.me.id);
+
+  const onToggleLike = useCallback(() => {
+    //이전 데이터를 기반으로 다음데이터를 만듦. 여기서는 true였으면 false로 false였으면 true로
+    setLiked((prev) => !prev);
+  }, []);
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+
+  return (
+    <div>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key='retweet' />,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor='#eb2f96'
+              key='heart'
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key='heart' onClick={onToggleLike} />
+          ),
+          <MessageOutlined key='comment' onClick={onToggleComment} />,
+          <Popover
+            key='more'
+            content={
+              <Button.Group>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button type='dancer'>삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
+      </Card>
+      {commentFormOpened && <div>댓글부분</div>}
+      {/* <CommentForm />
+      <COmments /> */}
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createAt: PropTypes.object,
+    Comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
 };
 
 export default PostCard;
---
+++ prepare/front/components/PostImages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line no-unused-vars
+const PostImages = ({ images }) => {
+  return <div>구현중...</div>;
+};
+
+PostImages.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default PostImages;
